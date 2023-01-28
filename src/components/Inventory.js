b740a1595ecd2ed0bba85d2722a8b2b647e8705e
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-01-28 12:20:03 -0800

    added components

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,54 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import {db} from "../firebase/firebase";
+import {collection, query, getDocs, addDoc} from "firebase/firestore"
 
-function Inventory() {
-  // Use the useState hook to set up state for the list of boxes
-  const [boxes, setBoxes] = useState(JSON.parse(localStorage.getItem('boxes')) || []);
+const Inventory = () => {
+  // State to store the inventory items
+  const [items, setItems] = useState([]);
 
-  // Function to add a new box to the list
-  const addBox = (length, width, height) => {
-    const newBox = { length, width, height };
-    setBoxes([...boxes, newBox]);
-    localStorage.setItem('boxes', JSON.stringify([...boxes, newBox]));
-  }
+  // Use effect to fetch the inventory items from Firestore
+  useEffect(() => {
+    const fetchItems = async () => {
+      const itemCollection = collection(db, "items");
+      const itemQuery = await query(itemCollection);
+      const itemDocs = await getDocs(itemQuery);
+      setItems(itemDocs.docs.map((doc) => doc.data()));
+    };
+    fetchItems();
+  }, []);
 
-  // Function to remove a box from the list
-  const removeBox = (index) => {
-    const newBoxes = [...boxes];
-    newBoxes.splice(index, 1);
-    setBoxes(newBoxes);
-    localStorage.setItem('boxes', JSON.stringify(newBoxes));
-  }
+  // Function to add a new item to the inventory
+  const addItem = async (name, quantity) => {
+    try {
+      await addDoc(collection(db, "items"), { name, quantity });
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  // Function to update an existing item in the inventory
+  const updateItem = async (id, name, quantity) => {
+    try {
+      const itemRef = collection(db, "items").doc(id);
+      await itemRef.update({ name, quantity });
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
+
+  // Function to delete an item from the inventory
+  const deleteItem = async (id) => {
+    try {
+      const itemRef = collection(db, "items").doc(id);
+      await itemRef.delete();
+    } catch (err) {
+      console.error(err);
+      alert(err.message);
+    }
+  };
 
   return (
     <div>
-      <h1>Shipping Box Inventory</h1>
-
-      <button onClick={() => addBox(5, 10, 15)}>Add Box</button>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Length</th>
-            <th>Width</th>
-            <th>Height</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {boxes.map((box, index) => (
-            <tr key={index}>
-              <td>{box.length}</td>
-              <td>{box.width}</td>
-              <td>{box.height}</td>
-              <td>
-                <button onClick={() => removeBox(index)}>Remove</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1>Inventory</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name}: {item.quantity}
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => addItem("New Item", 1)}>Add Item</button>
+      <button onClick={() => updateItem("itemId", "Updated Item", 2)}>
+        Update Item
+      </button>
+      <button onClick={() => deleteItem("itemId")}>Delete Item</button>
     </div>
   );
-}
+};
 
 export default Inventory;
