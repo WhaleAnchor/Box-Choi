743aa3b9c76059a675305dde478ce3912c66d4a7
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-02-08 13:42:47 -0800

    cleaning up unused files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import Inventory from './components/Inventory';
-import Navbar from './components/Navbar';
 import Login from './components/Login'
 import Register from './components/Register';
 import Reset from './components/Reset';
-import Dashboard from './components/Dashboard';
 import InventoryPage from './pages/InventoryPage';
 
 function App() {
@@ -18,7 +15,6 @@ function App() {
         <Route exact path="/" element={<Login />} />
         <Route exact path="/register" element={<Register />} />
         <Route exact path="/reset" element={<Reset />} />
-        <Route exact path="/dashboard" element={<Dashboard />} />
         <Route exact path="/inventory" element={<InventoryPage />} />
       </>
       </Routes>
