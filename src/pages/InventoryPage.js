3fdb1f2003d9e777f59d41edf3adec5f2613749f
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-02-08 13:16:34 -0800

    added components to the inventory page

---
+++ src/pages/InventoryPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Inventory from '../components/Inventory';
+
+const InventoryPage = () => {
+    return (
+        <>
+            <Navbar />
+            <Inventory />
+        </>
+    )
+};
+
+export default InventoryPage
