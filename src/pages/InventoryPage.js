4a220dd8cb2e6d0c434c77c1ec23e311d34495c8
Andrew Choi <andrewsungwoochoi@gmail.com>
2023-03-08 16:28:22 -0800

    added css and cleaned up js

--- src/pages/InventoryPage.js
+++ src/pages/InventoryPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Inventory from '../components/Inventory';
+import { Inventory2 } from '../components/Inventory2';
+import "./InventoryPage.css";
 
 const InventoryPage = () => {
     return (
         <>
             <Navbar />
-            <Inventory />
+            <Inventory2 />
         </>
     )
 };
